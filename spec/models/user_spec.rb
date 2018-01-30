8ab7791d9049d1c7aa716d00fda9c731d0b6b35d
Andrew Yee <andrewyee18331@sbcglobal.net>
2018-01-29 21:06:37 -0800

    Initialize user_spec with some test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = FactoryBot.build(:user) }
+
+  subject { @user }
+
+  # make sure that these are properties of the User model
+  it { should respond_to(:email) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+
+  # Passes if the given model instance's valid? method is true
+  # meaning all of the ActiveModel::Validations passed and no errors exist
+  it { should be_valid }
+
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { should validate_confirmation_of(:password) }
+  it { should allow_value('example@domain.com').for(:email) }
+
+  it { should have_many(:products) }
+  it { should have_many(:orders) }
 end
