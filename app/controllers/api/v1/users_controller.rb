83ec68b3ed44effed57c844d233eac79b2f71c3f
Andrew Yee <andrewyee18331@sbcglobal.net>
2018-01-29 23:33:22 -0800

    implement 'show' method for UsersController

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
+
+  def show
+    json_response(@user)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
