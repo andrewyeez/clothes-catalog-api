1fa60590a894bfa8ef2241dc2876a2f3f5308b95
Andrew Yee <andrewyee18331@sbcglobal.net>
2018-03-17 00:42:44 -0700

    error 'get#show' for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 require_relative '../lib/api_constraints'
 
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
+  devise_for :users
   # define api endpoints
-  namespace :api, dafaults: {format: :json}, 
+  namespace :api, dafaults: {format: :json},
                   contraints: {subdomain: 'api'},
-                  paths: '/' do
+                  path: '/' do
     scope module: :v1,
           constraints: ApiConstraints.new(version: 1, default: true) do
-      resource :users, :only => [:show]
+      resources :users, :only => [:show]
     end
   end
 end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -5,10 +5,9 @@ RSpec.describe Api::V1::UsersController, type: :controller do
 
   describe 'GET #show' do
     before(:each) do
-      @user = FactoryBot.create :user
-      get :show, id: @user.id, format: :json
+      @user = FactoryBot.create(:user)
+      get :show, :params => { :id => @user.id }, format: :json
     end
-
     it { should respond_with 200 }
   end
 end
