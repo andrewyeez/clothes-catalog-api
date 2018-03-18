b9542074bcc4cc44e833dcc6268819849894d9ba
Andrew Yee <andrewyee18331@sbcglobal.net>
2018-03-18 11:42:20 -0700

    postponing work on this

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
 
   def show
-    render json: @user, status: :ok
+    render json: @user, status: 200
   end
 
   private
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :controller do
-  before(:each) { request.headers['Accept'] = 'application/vnd.marketplace.v1' }
-
+  before(:each) {
+    request.headers['Accept'] = 'application/vnd.marketplace.v1'
+    @user = FactoryBot.create(:user)
+  }
   describe 'GET #show' do
-    before(:each) do
-      @user = FactoryBot.create(:user)
-      get :show, :params => { :id => @user.id }, format: :json
-    end
-    it { should respond_with 200 }
+    before { get :show, :params => { :id => @user.id }, format: :json }
+    it { should respond_with(200) }
   end
 end
